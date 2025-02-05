2698f9f0543695240992a293a6c557933776811c
Phetchomphu <2410511101067utcc@gmail.com>
2025-02-05 11:27:57 +0700

    refector#2 adding  paceCal fn.

--- common.js
+++ common.js
@@ -7,7 +7,15 @@ function validate(pace){
         msg = "Running pace must be between 3 and 10 min/km";
         return {"status":0,"message":msg,"hour":0,"min":0};
     }
-		
+}	
+function paceCal(pace,dist) {
+	var min = dist * pace;
+	var hour = 0;
+	if(min > 60){
+		hour = Math.floor(min / 60);          
+		min = min % 60;
+	}
+}
 	function calculate(p,d){
 		var msg = "";
 		var pace = parseFloat(p);
@@ -16,12 +24,7 @@ function validate(pace){
 	var chk = validate(pace);
 	if(chk != null) return chk;
 	//---------------------------
-		var min = dist * pace;
-		var hour = 0;
-		if(min > 60){
-			hour = Math.floor(min / 60);          
-			min = min % 60;
-		}
+	var pc = paceCal(pace,dist);
 	//---------------------------
 		msg = "successfully calculated!"
 		return {"status":1,"message":msg,"hour":hour,"min":min};
