7f9ecd2ed6dcf333fb768147d06517908a61a9bf
Phetchomphu <2410511101067utcc@gmail.com>
2025-02-05 11:29:39 +0700

    refector#3 modifyling the re turn message

--- common.js
+++ common.js
@@ -27,5 +27,5 @@ function paceCal(pace,dist) {
 	var pc = paceCal(pace,dist);
 	//---------------------------
 		msg = "successfully calculated!"
-		return {"status":1,"message":msg,"hour":hour,"min":min};
+		return {"status":1,"message":msg,"hour":pc.hour,"min":pc.min};
 }
